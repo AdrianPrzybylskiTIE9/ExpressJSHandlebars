ea41a2b22fc81493bba82071d154c05b2df48d9d
= <=>
2023-10-18 14:25:13 +0200

    Add pasta page

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const { engine } = require("express-handlebars");
+const pastaData = require("./public/data/pasta.json");
 
 const app = express();
 
@@ -10,8 +11,21 @@ app.set("views", "./views");
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.render("home");
+  res.render("home", {
+     pastaData: pastaData 
+  });
 });
+
+console.table(pastaData);
+console.log(pastaData["spaghetti"]);
+
+app.get("/pasta/:id", (req, res) => {
+    const { id }  = req.params;
+
+    res.render("pasta", { 
+      pastaData: pastaData[id]
+    })
+})
 
 const port = 8080 || env.port;
 app.listen(port, () => {
